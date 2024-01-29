feff55fbd2f5d2a4a74446b0a2bc595fa9d606f9
paumahiquesgarcia <92854877+paumahiquesgarcia@users.noreply.github.com>
2024-01-30 00:06:01 +0100

    final

---
+++ src/main/resources/test/java/AppTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(15, calculator.multiply(3, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivideByZero() {
+        calculator.divide(6, 0);
+    }
+}
