3007f888b9e696b66927d644ec3d0a58ab22f6b3
paumahiquesgarcia <92854877+paumahiquesgarcia@users.noreply.github.com>
2024-01-30 00:17:01 +0100

    d

--- src/main/resources/test/java/AppTest.java
+++ src/main/resources/test/java/AppTest.java
@@ -1,37 +1,17 @@
-package com.example;
-
+import com.example.Application;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 
 public class AppTest {
 
     @Test
-    public void testAdd() {
-        Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+    public void testApp() {
+        Application myApp = new Application();
+
+        String result = myApp.getStatus();
+
+        assertEquals("OK", result);
     }
 
-    @Test
-    public void testSubtract() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.subtract(5, 3));
-    }
-
-    @Test
-    public void testMultiply() {
-        Calculator calculator = new Calculator();
-        assertEquals(15, calculator.multiply(3, 5));
-    }
-
-    @Test
-    public void testDivide() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.divide(6, 3));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testDivideByZero() {
-        Calculator calculator = new Calculator();
-        calculator.divide(6, 0);
-    }
 }
